3fcd789649d6a84e87ad5a1f32d6233d7c50f3bd
Prerit Mogre <prerit.s.mogre@gmail.com>
2023-05-24 14:37:59 +0530

    Landing Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,21 @@ module.exports = {
         'true-teal': '#00B5B1',
         'bright-blue': '#0080FF',
         'orange-highlight': '#EF5B2B',
-        'new-gold': '#FFA800'
+        'new-gold': '#FFA800',
+        'amaranth': {
+          '50': '#fff1f4',
+          '100': '#ffe3e8',
+          '200': '#ffcbd8',
+          '300': '#ffa1b8',
+          '400': '#ff6d94',
+          '500': '#fa3972',
+          '600': '#e91f64',
+          '700': '#c40c4f',
+          '800': '#a40d49',
+          '900': '#8c0f45',
+          '950': '#4e0321',
+      },
+      
       },
       transitionProperty: {
         'width': 'width'
