b23cd5ad2c5408f8af05fc09a46d58f936aacc21
Prerit Mogre <prerit.s.mogre@gmail.com>
2023-06-22 15:14:10 +0530

    Admin console

--- src/components/Admin/ConsumerAdmin.js
+++ src/components/Admin/ConsumerAdmin.js
@@ -53,9 +53,31 @@ const ConsumerAdmin = () => {
   return (
     <div className="flex flex-col w-full">
       <div className="flex h-12 sticky top-0 z-30 py-2 bg-amaranth-800 flex-row items-center justify-between w-full">
-        <h3 className="px-5 text-lg font-light text-white">Consumer List</h3>
+        <h3 className="px-5 text-lg font-light text-white">Admin console</h3>
       </div>
-      <TableContainer className="mt-6">
+      {/* Create 3 tabs in tailwind css - 1. Consumer 2. Provider 3. Admin  */}
+      <div className="border-b border-b-gray-200">
+        <ul className="-mb-px flex items-center gap-4 text-sm font-medium">
+          <li className="flex-1">
+            <a href="#" className="flex items-center justify-center gap-2 px-1 py-3 text-gray-500 hover:text-amranth-700"> Profiles </a>
+          </li>
+          <li className="flex-1">
+            <a href="#" className="relative flex items-center justify-center gap-2 px-1 py-3 text-amranth-700 after:absolute after:bottom-0 after:left-0 after:h-0.5 after:w-full after:bg-blue-700 hover:text-blue-700"> Templates <span className="rounded-full bg-gray-100 px-2 py-0.5 text-xs font-semibold text-gray-500"> 2 </span></a>
+          </li>
+
+          <li className="flex-1">
+            <a href="#" className="flex items-center justify-center gap-2 px-1 py-3 text-gray-500 hover:text-amranth-700"> Itemised bills</a>
+          </li>
+         
+        </ul>
+
+      </div>
+
+      
+      <div>
+
+      </div>
+      <TableContainer className="mt-6 hidden">
         <Table
           sx={{ minWidth: 650, borderRadius: 0 }}
           stickyHeader
@@ -131,6 +153,53 @@ const ConsumerAdmin = () => {
           </TableBody>
         </Table>
       </TableContainer>
+
+      <div className="flex flex-col mt-4 px-5 w-1/3">
+        <div class="mt-2 pb-21 flex flex-col">
+          <label class="block text-sm font-medium leading-6 text-amaranth-600 ">Consumer name</label>
+          <input name="Column_Names" required="" class="bg-transparent  block w-full rounded-md border-0 py-1.5 text-amaranth-600  bg-blend-darken    shadow-sm ring-1 ring-inset ring-amaranth-600  placeholder:text-amaranth-600  focus:ring-2 focus:ring-inset focus:ring-amaranth-600  sm:text-sm sm:leading-6" />
+           
+          
+        </div>
+        <div class="settings">
+          {/* <span class="settings__title field-title">settings</span> */}
+          <div class="setting border-b border-b-gray-200">
+            <input type="checkbox" id="uppercase" />
+            <label for="uppercase">Query 1</label>
+          </div>
+          <div class="setting  border-b border-b-gray-200">
+            <input type="checkbox" id="lowercase"   />
+              <label for="lowercase">Query 2</label>
+          </div>
+          <div class="setting  border-b border-b-gray-200">
+            <input type="checkbox" id="number"   />
+              <label for="number">Query 3</label>
+          </div>
+          <div className="  border-b border-b-gray-200">
+            <div class="setting">
+              <input type="checkbox" id="symbol" checked />
+              <label for="symbol">Query 4</label>
+              
+            </div>
+            <ul className="pb-2">
+                <li className="ml-10">
+                  <input type="checkbox" id="sq1" />
+                  <label for="sq1" checked className="pl-2 text-gray-600">Age </label>
+                </li>
+                <li className="ml-10">
+                  <input type="checkbox" id="sq2" />
+                  <label for="sq2" className="pl-2 text-gray-600">Email </label>
+                </li>
+                <li className="ml-10">
+                  <input type="checkbox" id="sq3" />
+                  <label for="sq3" className="pl-2 text-gray-600">Overall </label>
+                </li>
+            </ul>
+          </div>
+         
+        </div>
+           
+      </div>
     </div>
   );
 };
