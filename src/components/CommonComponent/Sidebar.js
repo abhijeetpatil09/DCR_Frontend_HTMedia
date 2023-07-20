4d501fdceab76e173c827cbfdea62a218ecb3f92
Aniket Hase <ws_htu1260@hoonartek03.net>
2023-07-20 11:26:09 +0530

    Added Match rate and Sidebar

--- src/components/CommonComponent/Sidebar.js
+++ src/components/CommonComponent/Sidebar.js
@@ -308,33 +308,34 @@ const Sidebar = ({ children }) => {
                 </button>
               </li>
             )}
-
-            <li
-              className={`${
-                tab === 7 ? "bg-amaranth-200" : ""
-              } hover:bg-amaranth-200 transition ease-in-out duration-500`}
-            >
-              <button
-                onClick={() => navigateTo("/querystatus")}
-                className=" py-4 px-6 flex flex justify-start items-center w-full  capitalize font-medium text-sm "
+            {user["role"] && !user["role"].includes("Provider") && (
+              <li
+                className={`${
+                  tab === 7 ? "bg-amaranth-200" : ""
+                } hover:bg-amaranth-200 transition ease-in-out duration-500`}
               >
-                <i className="text-xs mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="w-4 h-4"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-13a.75.75 0 00-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 000-1.5h-3.25V5z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </i>
-                <span className={`${isOpened ? "" : "hidden"} `}>Status</span>
-              </button>
-            </li>
+                <button
+                  onClick={() => navigateTo("/querystatus")}
+                  className=" py-4 px-6 flex flex justify-start items-center w-full  capitalize font-medium text-sm "
+                >
+                  <i className="text-xs mr-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-4 h-4"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-13a.75.75 0 00-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 000-1.5h-3.25V5z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </i>
+                  <span className={`${isOpened ? "" : "hidden"} `}>Status</span>
+                </button>
+              </li>
+            )}
           </ul>
         </aside>
 
--- src/components/MatchRate.js
+++ src/components/MatchRate.js
@@ -842,39 +842,47 @@ const MatchRate = () => {
                           </svg>
                         </button>
                       )}
-                      <button
-                        onClick={() => handleUploadData(item.RUN_ID)}
-                        disabled={
-                          item.UPL_INTO_CLI_SPACE?.toLowerCase() === "true" &&
-                          item.STATUS?.toLowerCase() === "completed"
-                        }
-                        className={`${
-                          item.UPL_INTO_CLI_SPACE?.toLowerCase() !== "true" &&
-                          item.STATUS?.toLowerCase() === "completed"
-                            ? "opacity-1 hover:text-inherit"
-                            : "disabled opacity-10 hover:text-inherit"
-                        }  px-2 hover:text-amaranth-600`}
-                        title={
-                          item.UPL_INTO_CLI_SPACE?.toLowerCase() === "true"
-                            ? "Already Uploaded into client ecospace"
-                            : "Upload match records into client ecospace"
-                        }
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="w-5 h-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-                          />
-                        </svg>
-                      </button>
+                      {user.role &&
+                      user?.role?.includes("Publisher") &&
+                      user?.role?.includes("Consumer") ? (
+                        <>
+                          <button
+                            onClick={() => handleUploadData(item.RUN_ID)}
+                            disabled={
+                              item.UPL_INTO_CLI_SPACE?.toLowerCase() ===
+                                "true" &&
+                              item.STATUS?.toLowerCase() === "completed"
+                            }
+                            className={`${
+                              item.UPL_INTO_CLI_SPACE?.toLowerCase() !==
+                                "true" &&
+                              item.STATUS?.toLowerCase() === "completed"
+                                ? "opacity-1 hover:text-inherit"
+                                : "disabled opacity-10 hover:text-inherit"
+                            }  px-2 hover:text-amaranth-600`}
+                            title={
+                              item.UPL_INTO_CLI_SPACE?.toLowerCase() === "true"
+                                ? "Already Uploaded into client ecospace"
+                                : "Upload match records into client ecospace"
+                            }
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                              className="w-5 h-5"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                              />
+                            </svg>
+                          </button>
+                        </>
+                      ) : null}
                     </div>
                   </td>
                 </tr>
