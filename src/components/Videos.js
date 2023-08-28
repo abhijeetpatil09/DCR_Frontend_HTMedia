ae3aadaa2367dcf89fbbd44f9f3ca5adb38dd0a3
Aniket Hase <ws_htu1260@hoonartek03.net>
2023-08-28 19:12:17 +0530

    Added changes for Videos

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import video1 from "../Assets/HowToVideos/DataCleanRoom_video.mp4";
 import video2 from "../Assets/HowToVideos/Login_Match_Rate.mp4";
 import video3 from "../Assets/HowToVideos/Customer_Enrichment.mp4";
@@ -7,6 +7,30 @@ import { useNavigate } from "react-router-dom";
 
 const Videos = () => {
   const navigate = useNavigate();
+  const [currentVideo, setCurrentVideo] = useState(-1);
+
+  const videoRefs = [useRef(), useRef(), useRef(), useRef()];
+
+  const playVideo = (index) => {
+    if (currentVideo !== index) {
+      if (currentVideo !== -1) {
+        videoRefs[currentVideo].current.pause();
+      }
+      setCurrentVideo(index);
+      videoRefs[index].current.play();
+    }
+  };
+
+  const handleVideoPlay = (index) => {
+    playVideo(index);
+  };
+
+  const handleVideoPause = () => {
+    if (currentVideo !== -1) {
+      videoRefs[currentVideo].current.pause();
+      setCurrentVideo(-1);
+    }
+  };
 
   return (
     <div className="w-full max-w-full px-3 sm:flex-0 shrink-0 sm:w-6/12 lg:w-full">
@@ -36,14 +60,20 @@ const Videos = () => {
             <li className="relative flex justify-between py-2 pr-4 mb-2 border-0 rounded-t-lg rounded-xl text-inherit">
               <div className="flex items-center w-1/2">
                 <div className="flex-auto p-4">
-                  <h2 className="font-semibold mb-4 text-amaranth-700">Introduction to DCR</h2>
+                  <h2 className="font-semibold mb-4 text-amaranth-700">
+                    Introduction to DCR
+                  </h2>
                   <video
+                    ref={videoRefs[0]}
                     width="100%"
                     height="240"
                     loop
                     muted
                     controls
                     className="w-full rounded-t-lg rounded-b-lg"
+                    onPlay={() => handleVideoPlay(0)}
+                    onPause={handleVideoPause}
+                    onEnded={handleVideoPause}
                   >
                     <source src={video1} type="video/mp4" />
                     Your browser does not support the video tag.
@@ -54,14 +84,20 @@ const Videos = () => {
 
               <div className="flex items-center w-1/2 ml-2">
                 <div className="flex-auto p-4">
-                  <h2 className="font-semibold mb-4 text-amaranth-700">Introduction to Log in and Match Rate feature</h2>
+                  <h2 className="font-semibold mb-4 text-amaranth-700">
+                    Introduction to Log in and Match Rate feature
+                  </h2>
                   <video
+                    ref={videoRefs[1]}
                     width="100%"
                     height="240"
                     loop
                     muted
                     controls
                     className="w-full rounded-t-lg rounded-b-lg"
+                    onPlay={() => handleVideoPlay(1)}
+                    onPause={handleVideoPause}
+                    onEnded={handleVideoPause}
                   >
                     <source src={video2} type="video/mp4" />
                     Your browser does not support the video tag.
@@ -74,14 +110,20 @@ const Videos = () => {
             <li className="relative flex justify-between py-2 pr-4 mb-2 mt-5 border-0 rounded-t-lg rounded-xl text-inherit">
               <div className="flex items-center w-1/2">
                 <div className="flex-auto p-4">
-                  <h2 className="font-semibold mb-4 text-amaranth-700">Introduction to Customer Enrichment feature</h2>
+                  <h2 className="font-semibold mb-4 text-amaranth-700">
+                    Introduction to Customer Enrichment feature
+                  </h2>
                   <video
+                    ref={videoRefs[2]}
                     width="100%"
                     height="240"
                     loop
                     muted
                     controls
                     className="w-full rounded-t-lg rounded-b-lg"
+                    onPlay={() => handleVideoPlay(2)}
+                    onPause={handleVideoPause}
+                    onEnded={handleVideoPause}
                   >
                     <source src={video3} type="video/mp4" />
                     Your browser does not support the video tag.
@@ -92,14 +134,20 @@ const Videos = () => {
 
               <div className="flex items-center w-1/2 ml-2">
                 <div className="flex-auto p-4">
-                  <h2 className="font-semibold mb-4 text-amaranth-700" >Introduction to Admin console and status feature</h2>
+                  <h2 className="font-semibold mb-4 text-amaranth-700">
+                    Introduction to Admin console and status feature
+                  </h2>
                   <video
+                    ref={videoRefs[3]}
                     width="100%"
                     height="240"
                     loop
                     muted
                     controls
                     className="w-full rounded-t-lg rounded-b-lg"
+                    onPlay={() => handleVideoPlay(3)}
+                    onPause={handleVideoPause}
+                    onEnded={handleVideoPause}
                   >
                     <source src={video4} type="video/mp4" />
                     Your browser does not support the video tag.
